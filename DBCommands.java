0a85337a4de7422b6df4243adf537174013d4b3f
tyty4646 <tyty464646@gmail.com>
2023-04-18 16:29:13 -0400

    battery going to die

--- DBCommands.java
+++ DBCommands.java
@@ -13,45 +13,58 @@ public class DBCommands {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = DriverManager.getConnection(dbUrl, username, password);
 			if(conn != null) System.out.println("You are connected!");
+
+			conn.setAutoCommit(false);
+   			
+			switch (args[0]) {
+				default:
+					System.out.println("Invalid Command");
+					break;
+				case "test":
+					System.out.println(test() ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addCustomer": 
+					java.sql.Date dateOfBirth = java.sql.Date.valueOf(args[5]);
+					System.out.println(addCustomer(conn, Integer.valueOf(args[1]), args[2], args[3], args[4], dateOfBirth) ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addPolicy":
+					java.sql.Date startDate = java.sql.Date.valueOf(args[4]);
+					java.sql.Date endDate = java.sql.Date.valueOf(args[5]);
+					System.out.println(addPolicy(Integer.valueOf(args[1]), args[2], Integer.valueOf(args[3]),
+						startDate, endDate, args[6]) ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addCarInfo":
+					System.out.println(addCarInfo(Integer.valueOf(args[1]), Integer.valueOf(args[2]), args[3], args[4],
+						Integer.valueOf(args[5]), Integer.valueOf(args[6])) ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addHomeInfo":
+					System.out.println(addHomeInfo(Integer.valueOf(args[1]), args[2], Integer.valueOf(args[3]), Integer.valueOf(args[4]),
+						Integer.valueOf(args[5]), Integer.valueOf(args[6]), Integer.valueOf(args[7])) ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addLifeInfo":
+					System.out.println(addLifeInfo(Integer.valueOf(args[1]), Integer.valueOf(args[2]), args[3]) ? "Command Completed Successfully" : "Command Failed");
+					break;
+				case "addConditions":
+					System.out.println(addConditions(Integer.valueOf(args[1]), args[2]) ? "Command Completed Successfully" : "Command Failed");
+					break;
+			}
 		}
 		catch (SQLException error) {
 			error.printStackTrace();
 		}
-
-		switch (args[0]) {
-			case "test":
-				System.out.println(test() ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addPerson": 
-				java.sql.Date dateOfBirth = java.sql.Date.valueOf(args[5]);
-				System.out.println(addPerson(Integer.valueOf(args[1]), args[2], args[3], args[4], dateOfBirth) ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addPolicy":
-				java.sql.Date startDate = java.sql.Date.valueOf(args[4]);
-				java.sql.Date endDate = java.sql.Date.valueOf(args[5]);
-				System.out.println(addPolicy(Integer.valueOf(args[1]), args[2], Integer.valueOf(args[3]),
-					startDate, endDate, args[6]) ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addCarInfo":
-				System.out.println(addCarInfo(Integer.valueOf(args[1]), Integer.valueOf(args[2]), args[3], args[4],
-					Integer.valueOf(args[5]), Integer.valueOf(args[6])) ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addHomeInfo":
-				System.out.println(addHomeInfo(Integer.valueOf(args[1]), args[2], Integer.valueOf(args[3]), Integer.valueOf(args[4]),
-					Integer.valueOf(args[5]), Integer.valueOf(args[6]), Integer.valueOf(args[7])) ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addLifeInfo":
-				System.out.println(addLifeInfo(Integer.valueOf(args[1]), Integer.valueOf(args[2]), args[3]) ? "Command Completed Successfully" : "Command Failed");
-				break;
-			case "addConditions":
-				System.out.println(addConditions(Integer.valueOf(args[1]), args[2]) ? "Command Completed Successfully" : "Command Failed");
-				break;
-		}
 	}
 
-	public static boolean addPerson(int SSN, String firstName, String lastName, String contactInfo, java.sql.Date dateOfBirth) {
-
-		return false;
+	public static boolean addCustomer(Connection conn, int SSN, String firstName, String lastName, String contactInfo, java.sql.Date dateOfBirth) {
+		try {
+			Statement stmt = conn.createStatement();
+			String query = "INSERT INTO Customer VALUES (" + SSN + ", '" + firstName+ "', '" + lastName + "', '" + contactInfo + "', " + dateOfBirth + ");";
+			System.out.println(query);
+			stmt.executeUpdate(query);  
+			return true;
+		} catch (SQLException error) {
+			error.printStackTrace();
+			return false;
+		}
 	}
 
 	public static boolean addPolicy(int policyID, String coverage, int monthlyPayment, java.sql.Date startDate, java.sql.Date endDate, String owner) {
@@ -77,6 +90,16 @@ public class DBCommands {
 	public static boolean addConditions(int lifeID, String condition) {
 
 		return false;
+	}
+
+	public static String listPeople() {
+
+		return "";
+	}
+
+	public static String listPolicies() {
+
+		return "";
 	}
 
 	public static boolean test() {
